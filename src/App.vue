a215a31b44321d4c4f4ad4c42f46a8aca8c87826
manbalboy <manbalboy@hanmail.net>
2021-03-09 22:44:15 +0900

    Tab 구현

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,29 @@
         <Modal v-if="showModal" @close="showModal = false">
             <h3 slot="header" style="color: black">ttt</h3>
         </Modal>
+        <TabsA :tapList="listA"></TabsA>
     </div>
 </template>
 
 <script>
     // import CommonHeader from '@/components/CommonHeader.vue';
     import Modal from '@/components/Modal.vue';
+    import TabsA from '@/components/Tab/TabsA.vue';
     export default {
         data() {
-            return { showModal: false };
+            return {
+                showModal: false,
+
+                listA: [
+                    { id: 1, label: 'Tab1', content: '콘텐츠1' },
+                    { id: 2, label: 'Tab2', content: '콘텐츠2' },
+                    { id: 3, label: 'Tab3', content: '콘텐츠3' },
+                ],
+            };
         },
         name: 'App',
         components: {
+            TabsA,
             // CommonHeader,
             Modal,
         },
---
+++ src/components/Tab/TabsA.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="example">
+        <div class="tabs">
+            <TabItem v-for="item in list" v-bind="item" :key="item.id" v-model="currentId" />
+        </div>
+        <div class="contents">
+            <section class="item">
+                {{ current.content }}
+            </section>
+        </div>
+    </div>
+</template>
+
+<script>
+    import TabItem from './TabItem.vue';
+    export default {
+        name: 'tab',
+        components: { TabItem },
+        data() {
+            return {
+                currentId: 1,
+                list: [{}],
+            };
+        },
+        props: ['tapList'],
+        computed: {
+            current() {
+                return this.list.find(item => item.id === this.currentId) || {};
+            },
+        },
+        mounted() {
+            this.list = this.tapList;
+        },
+    };
+</script>
+
+<style scoped>
+    .contents {
+        position: relative;
+        overflow: hidden;
+        width: 280px;
+        border: 2px solid #000;
+    }
+    .item {
+        box-sizing: border-box;
+        padding: 10px;
+        width: 100%;
+        transition: all 0.8s ease;
+    }
+    /* 트랜지션 전용 스타일 */
+    /* .v-leave-active {
+        position: absolute;
+    }
+    .v-enter {
+        transform: translateX(-100%);
+    }
+    .v-leave-to {
+        transform: translateX(100%);
+    } */
+</style>
