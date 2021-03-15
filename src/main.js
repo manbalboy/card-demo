26fd23efb3177268e0fce044fdf896c18bab7e19
manbalboy <manbalboy@hanmail.net>
2021-03-15 21:24:51 +0900

    vue - cookies  학습

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import App from './App.vue';
 import Layout from '@/layouts/Layout.vue';
 import router from '@/router/index.js';
 import store from '@/store/index.js';
-
+import VueCookies from 'vue-cookies';
+Vue.use(VueCookies);
 Vue.component('Layout', Layout);
 Vue.config.productionTip = false;
 
