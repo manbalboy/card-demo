a05e160737076c172d08b687a8b89f7994358675
manbalboy <manbalboy@hanmail.net>
2021-03-15 21:24:19 +0900

    vue-cookies

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,8 @@
         <Layout>
             <router-view />
         </Layout>
+        <button @click="cookiCreate">쿠키생성</button>
+        <button @click="cookiDelete">쿠키생성</button>
     </div>
 </template>
 
@@ -10,25 +12,41 @@
     // import CommonHeader from '@/components/CommonHeader.vue';
     // import Modal from '@/components/Modal.vue';
     // import TabsA from '@/components/Tab/TabsA.vue';
-    // export default {
-    //     data() {
-    //         return {
-    //             showModal: false,
+    export default {
+        methods: {
+            cookiCreate() {
+                //만료일은 7일이다 전역 셋팅
+                // 1 : 1초
+                // 60 * 60 * 12 : 12시간
+                // 60 * 60 * 25 * 30 : 1달
+                // 1d : 1일
+                // new Date(2019,03,13).toUTCString(): 특정 시간
+                this.$cookies.config('7d');
 
-    //             listA: [
-    //                 { id: 1, label: 'Tab1', content: '콘텐츠1' },
-    //                 { id: 2, label: 'Tab2', content: '콘텐츠2' },
-    //                 { id: 3, label: 'Tab3', content: '콘텐츠3' },
-    //             ],
-    //         };
-    //     },
-    //     name: 'App',
-    //     components: {
-    //         TabsA,
-    //         // CommonHeader,
-    //         Modal,
-    //     },
-    // };
+                // 만료일이 명시되지 않는다면 글로벌 만료일이 저장됨
+                // this.$cookies.set('키', '값', '만료일');
+                this.$cookies.set('TTT', 'test', '60');
+
+                const cookie = this.$cookies.get('TTT');
+                console.log(cookie);
+            },
+
+            cookiDelete() {
+                this.$cookies.remove('TTT');
+                const cookie = this.$cookies.get('TTT');
+                console.log(cookie);
+
+                //특정 쿠키가 있는지 확인
+                this.$cookies.isKey('test');
+
+                //모든 쿠키 키 가져오기
+                this.$cookies.keys().join('\n');
+
+                //모든 쿠키 다 지우기
+                this.$cookies.keys().forEach(cookie => this.$cookies.remove(cookie));
+            },
+        },
+    };
 </script>
 
 <style></style>
