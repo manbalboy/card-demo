f93efa170c7ef807ca3d7052894cd37aa8efcdd1
manbalboy <manbalboy@hanmail.net>
2021-03-24 00:28:12 +0900

    vue render 함수

---
+++ src/modules/card/pages/Test.vue
@@ -0,0 +1,20 @@
+<script>
+    export default {
+        render: h =>
+            h(
+                'div',
+                {
+                    class: {
+                        test: true,
+                    },
+                },
+                [h('div', 'afdsa')],
+            ),
+    };
+</script>
+
+<style>
+    .test {
+        background-color: red;
+    }
+</style>
--- src/modules/card/router/card-router.js
+++ src/modules/card/router/card-router.js
@@ -4,6 +4,11 @@ const cardRouter = [
         name: 'CardMain',
         component: () => import('../pages/CardMain.vue'),
     },
+    {
+        path: '/card-test',
+        name: 'test',
+        component: () => import('../pages/Test.vue'),
+    },
 ];
 
 export default cardRouter;
